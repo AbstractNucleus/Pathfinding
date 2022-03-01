dbe4132ac83f8f77451ea60d0d20d0d3fe3a237b
AbstractNucleus <noelhkleen@gmail.com>
2022-03-01 10:48:34 +0100

    node classs

--- src/classes.py
+++ src/classes.py
@@ -1,29 +1,37 @@
 import math
 
 class Node():
-    def __init__(self, pos=None, parent=None):
+    def __init__(self, pos=None, parent=None,):
         self.pos = pos
         self.parent = parent
         self.h = H(self.pos)
         self.g = 0
         self.f = self.g + self.h
+        self.wall = True
 
-    def H(self):
-        return math.sqrt(self.pos[0]^2 + self.pos[1]^2)
+    def H(self): return math.sqrt(self.pos[0]^2 + self.pos[1]^2)
     
-    def get_position(self):
-        return self.pos
+    def get_position(self): return self.pos
     
-    def get_parent(self):
-        return self.parent
+    def get_parent(self): return self.parent
 
-board = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    def get_neighboors(self):
+        return [
+                (self.pos[0]+1, self.pos[1]), 
+                (self.pos[0]-1, self.pos[1]),
+                (self.pos[0], self.pos[1]+1),
+                (self.pos[0], self.pos[1]-1)
+                ]
+
+board = [
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]]
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+        ]
 
 start = (0, 0)
 end = (6, 6)
